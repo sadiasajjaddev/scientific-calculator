28fe3c9b63ef9fb84cb3cc523c3d63e9895b7ea7
sadiasajjad <125554790+sadiasajjaddev@users.noreply.github.com>
2023-05-22 14:43:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 function getNum(num){
-    document.getElementById('result').value += num;
-}
+   var resultinput= document.getElementById('result');
 if (num === "Math.sin") {
   resultInput.value = resultInput.value.replace(/(.*)/, "sin");
-
-} 
-else if (num === "Math.cos") {
+} else if (num === "Math.cos") {
   resultInput.value = resultInput.value.replace(/(.*)/, "cos");
-} 
-  else if (num === "Math.E") {
+}  else if (num === "Math.E") {
   resultInput.value = resultInput.value.replace(/(.*)/, "E");
-} 
-else if (num === "Math.tan") {
+} else if (num === "Math.tan") {
   resultInput.value = resultInput.value.replace(/(.*)/, "tan");
-}
- else if (num === "Math.PI") {
+} else if (num === "Math.PI") {
   resultInput.value = resultInput.value.replace(/(.*)/, "π");
-} 
-else if (num === "Math.log") {
+} else if (num === "Math.log") {
   resultInput.value = resultInput.value.replace(/(.*)/, "log");
-}
- else if (num === "Math.exp") {
+} else if (num === "Math.exp") {
     resultInput.value = resultInput.value.replace(/(.*)/, "exp");
-}
-else if (num === "Math.sqrt") {
+}else if (num === "Math.sqrt") {
     resultInput.value = "√"; resultInput.value += num;
 }  
 function clearscr(){
-    document.getElementById('result').value = '';
+    document.getElementById("result").value = "";
 }
-
 function removeNumb(){
-    var res = document.getElementById('result').value; 
-    document.getElementById('result').value = document.getElementById('result').value.slice(0,res.length-1)
+    var res = document.getElementById('result").value; 
+    document.getElementById("result").value = document.getElementById("result").value.slice(0,res.length-1)
 }
-
-function getAns(){
-    document.getElementById('result').value = eval(document.getElementById('result').value)
-
+function getresults(){
+   var resultinput=document.getElementById("result");
+    var expression=resultinput.valur;
 }
  try {
     var result = eval(expression);
